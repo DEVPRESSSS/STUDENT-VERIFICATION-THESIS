8756e1857051b5dcd7c04c70259571d1d1a03410
devpresss xmont <xmontemorjerald@gmail.com>
2024-12-09 18:50:11 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace STUDENT_VERIFICATION_SYSTEM_THIRD_YEAR_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
