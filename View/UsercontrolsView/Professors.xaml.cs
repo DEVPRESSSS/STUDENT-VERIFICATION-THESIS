08bd1a9086b80927ad2efdb0911429e64b3e6046
devpresss xmont <xmontemorjerald@gmail.com>
2024-12-23 00:10:21 +0800

    Added professor form design

--- View/UsercontrolsView/Professors.xaml.cs
+++ View/UsercontrolsView/Professors.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using STUDENT_VERIFICATION_SYSTEM_THIRD_YEAR_PROJECT.View.PopUpForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,13 @@ namespace STUDENT_VERIFICATION_SYSTEM_THIRD_YEAR_PROJECT.View.UsercontrolsView
         private void UsernameXZ_GotFocus(object sender, RoutedEventArgs e)
         {
             UsernameXZ.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            AddProfessorForm s = new AddProfessorForm();
+            s.ShowDialog();
         }
     }
 }
